c2a5bf9f89ec16ffbcc4af6c715a8356efee8038
guillermomezzano <102881932+guillermomezzano@users.noreply.github.com>
2024-03-25 13:35:11 -0300

    add new controllers(create, delete, update, read) and routes

--- controllers/student.controllers.js
+++ controllers/student.controllers.js
@@ -11,9 +11,58 @@ module.exports.findAllStudent = (req, res) => {
     );
 };
 
+// module.exports.createStudent = (req, res) => {
+//   Student.create(req.body)
+//     .then((newStudent) => res.json({ newStudent }))
+//     .catch((err) =>
+//       res.json({
+//         message: "no hemos podido encontrar los estudiantes",
+//         error: err,
+//       })
+//     );
+// };
+
+module.exports.createStudent = async (req, res) => {
+  try {
+    const newStudent = await Student.create(req.body);
+    res.json({ newStudent });
+  } catch (error) {
+    res.json({
+      message: "no hemos podido encontrar los estudiantes",
+      error: err,
+    });
+  }
+};
+
 module.exports.findOneStudent = (req, res) => {
-  Student.find()
-    .then((allStudent) => res.json({ allStudent }))
+  Student.findOne({ _id: req.params.id })
+    .then((findOneStudent) => res.json({ findOneStudent }))
+    .catch((err) =>
+      res.json({
+        message: "no hemos podido encontrar los estudiantes",
+        error: err,
+      })
+    );
+};
+
+module.exports.updateStudent = (req, res) => {
+  Student.findOneAndUpdate(
+    { _id: req.params.id },
+    { nombre: req.body.nombre },
+    { new: true }
+  )
+    .then((updateStudent) => res.json({ updateStudent }))
+    .catch((err) =>
+      res.json({
+        message: "no hemos podido encontrar los estudiantes",
+        error: err,
+      })
+    );
+};
+
+module.exports.deleteStudent = (req, res) => {
+  Student.deleteOne({ _id: req.params.id })
+    .then((deleteStudent) => res.json({ deleteStudent }))
     .catch((err) =>
       res.json({
         message: "no hemos podido encontrar los estudiantes",
--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,8 +1,15 @@
 const {
   findAllStudent,
+  createStudent,
+  updateStudent,
+  deleteStudent,
   findOneStudent,
 } = require("../controllers/student.controllers");
 
 module.exports = (app) => {
-  app.get("/student", findAllStudent);
+  app.get("/students", findAllStudent);
+  app.post("/student", createStudent);
+  app.put("/student/:id", updateStudent);
+  app.delete("/student/:id", deleteStudent);
+  app.get("/student/:id", findOneStudent);
 };
